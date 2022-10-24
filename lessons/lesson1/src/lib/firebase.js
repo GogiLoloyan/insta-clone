51092727c2ee4b3174611064463b1ecb93fd405e
Gogi Loloyan <Gogi_Loloyan@epam.com>
2022-10-24 10:17:27 +0400

    lesson-1

---
+++ lessons/lesson1/src/lib/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { FieldValue } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTrG0zdk20x--O-84dcS5hFgtd_r7SPZ8",
+    authDomain: "insta-clone-e9833.firebaseapp.com",
+    databaseURL: "https://insta-clone-e9833-default-rtdb.firebaseio.com",
+    projectId: "insta-clone-e9833",
+    storageBucket: "insta-clone-e9833.appspot.com",
+    messagingSenderId: "677644911091",
+    appId: "1:677644911091:web:fda8b70df2fef2e4295286"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export { firebaseApp, FieldValue };
