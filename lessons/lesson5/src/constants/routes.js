2144409887b894f271b5570643098c4dde6d6f48
Gogi Loloyan <Gogi_Loloyan@epam.com>
2022-11-21 20:00:37 +0400

    remove useless folders

--- lessons/lesson5/src/constants/routes.js
+++
@@ -1,7 +0,0 @@
-export const HOME = '/';
-export const LOGIN = '/login';
-export const SIGN_UP = '/signup';
-export const DASHBOARD = '/dashboard'
-export const NOT_FOUND = '/not-found'
-export const PROFILE = "/profile/:username";
-export const PROFILE_USER_NAME_PARAM = ":username";
--- lessons/lesson5/src/hooks/useSiteTitle.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-
-export const useSiteTitle = (title) => {
-    useEffect(() => {
-        document.title = `${title} | Instagram`;
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-};
